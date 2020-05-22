f42360046b7c7093d87c07e2ce5c39be3b3836d7
Ri <syairidev@gmail.com>
2020-05-23 04:05:55 +0800

    Updates

---
+++ precache-manifest.a2fad24185324e305ddcd7318c7d4f05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd76c21c8e884caee323261110bc5d0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e103a35e81669b2f470e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "02bf0a1b7f278c16a550",
+    "url": "/robofriends/static/css/main.62b667db.chunk.css"
+  },
+  {
+    "revision": "e103a35e81669b2f470e",
+    "url": "/robofriends/static/js/2.a7791816.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a7791816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02bf0a1b7f278c16a550",
+    "url": "/robofriends/static/js/main.3f210673.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "b3a7bd4b0f875d4af62c56a9ed218ca7",
+    "url": "/robofriends/static/media/BIONICKIDSLANTED3D.b3a7bd4b.ttf"
+  }
+]);
